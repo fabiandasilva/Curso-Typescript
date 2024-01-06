f3002068141afbe593bb9bf803b2ee9253a5b501
Fabian Da Silva <fabian.da.silva@hotmail.com>
2024-01-06 01:16:37 -0300

    Sección 3: Tipos avanzados

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,73 @@
 "use strict";
-let pato = 'pato';
-pato = "gatito";
-console.log(pato);
+let extincionDinosaurios = 65000000;
+let dinosaurioFavorito = "Velociraptor";
+let extintos = true;
+let miVariable;
+function saludar(config) {
+    return config;
+}
+let animales = ["Leon", "Tigre", "Elefante"];
+let numbers = [1, 2, 3, 4, 5];
+let numbersTwo = [];
+let numbersThree = [];
+let tupla = [1, "hola", true];
+const chica = "s";
+const mediana = "m";
+const grande = "l";
+const extraGrande = "xl";
+var Talla;
+(function (Talla) {
+    Talla["Chica"] = "s";
+    Talla["Mediana"] = "n";
+    Talla["Grande"] = "l";
+    Talla["ExtraGrande"] = "xl";
+})(Talla || (Talla = {}));
+const variable1 = Talla.Chica;
+console.log(variable1);
+var LoadingState;
+(function (LoadingState) {
+    LoadingState[LoadingState["Idle"] = 0] = "Idle";
+    LoadingState[LoadingState["Loading"] = 1] = "Loading";
+    LoadingState[LoadingState["Success"] = 2] = "Success";
+    LoadingState[LoadingState["Failed"] = 3] = "Failed";
+})(LoadingState || (LoadingState = {}));
+const estado = LoadingState.Idle;
+const objeto = {
+    id: 1,
+    nombre: "Hola mundo",
+    talla: Talla.Chica,
+    direccion: {
+        numero: 123,
+        calle: "Calle falsa",
+        pais: "Mexico"
+    }
+};
+const arr = [];
+const fn = (edad) => {
+    if (edad > 17)
+        return 'puedes ingresar';
+    return 'no puedes ingresar';
+};
+function validadEdad(edad) {
+    if (edad > 17)
+        return 'puedes ingresar';
+    return 'no puedes ingresar';
+}
+function ErrorUsuario(mensaje) {
+    throw new Error(mensaje);
+}
+let puntaje = 98;
+puntaje = "98";
+let animal = {
+    id: 1,
+    estado: "",
+    nombre: ""
+};
+function sumaDos(n) {
+    if (typeof n === "number") {
+        return n + 2;
+    }
+    return parseInt(n) + 2;
+}
+sumaDos("10");
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-/* TIPOS BASICOS */
+/* Sección 2: Tipos básicos */
 
 //? Boolean, Number, String, Array, Tuple, Enum, Any, Void, Null, Undefined, Never, Object
 
@@ -18,3 +18,104 @@ let numbersTwo: boolean[] = [];
 let numbersThree: Array<number> = [];
 
 // numbers.map((x) =>x.) El autocomplete funciona con los tipos de datos
+
+let tupla: [number, string, boolean] = [1, "hola", true];
+
+const chica = "s"
+const mediana = "m"
+const grande = "l"
+const extraGrande = "xl"
+
+//Pascale Case
+enum Talla {
+  Chica = "s", Mediana = "n", Grande = "l", ExtraGrande = "xl"
+}
+
+const variable1 = Talla.Chica;
+console.log(variable1);
+
+enum LoadingState {
+  Idle, Loading, Success, Failed
+}
+
+
+const estado = LoadingState.Idle;
+
+type Direccion = {
+  numero: number,
+  calle: string
+  pais: string
+}
+
+type Persona = {
+  readonly id: number,
+  nombre: string,
+  talla: Talla,
+  direccion: Direccion
+}
+
+const objeto: Persona = {
+  id: 1,
+  nombre: "Hola mundo",
+  talla: Talla.Chica,
+  direccion: {
+    numero: 123,
+    calle: "Calle falsa",
+    pais: "Mexico"
+  }
+}
+
+const arr:Persona[] = []
+
+const fn: (a: number) => string = (edad:number) => {
+  if (edad > 17)
+    return 'puedes ingresar'  
+  return 'no puedes ingresar'
+}
+
+function validadEdad (edad: number): string{
+  if(edad > 17)
+    return 'puedes ingresar'
+  return 'no puedes ingresar'
+}
+
+
+function ErrorUsuario(mensaje: string): never {
+  throw new Error(mensaje);
+}
+
+/* Sección 3: Tipos avanzados */
+
+//? Union, Alias, Intersection, Literal, Guards, Type Inference, Type Assertion
+
+/* Union type */
+let puntaje: number | string = 98;
+
+puntaje = "98";
+
+type Animal={
+  id: number,
+  estado: string
+}
+
+
+
+type Usuario={
+  id: number,
+  nombre: string
+}
+
+let animal: Usuario | Animal = {
+  id: 1,
+  estado:"",
+  nombre: ""
+}
+
+function sumaDos(n: number | string): number {
+  if (typeof n === "number") {    
+    return n + 2;
+  }
+  return parseInt(n) + 2;
+}
+
+sumaDos("10");
